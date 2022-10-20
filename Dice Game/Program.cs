e6eb5d2396000a9b78f798097908bb4fef63b11d
hugo-lcm <hugolcm@live.com>
2022-10-20 14:50:59 -0300

    Add project files.

---
+++ Dice Game/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace Dice_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("teste");
+        }
+    }
+}
